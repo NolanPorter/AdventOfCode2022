67fdc21842b53d61f4a7420d7bdaacb9ac291442
Nolan Porter <porternolan62@gmail.com>
2022-12-05 20:10:00 -0500

    day 5

---
+++ Question 5/q5.py
@@ -0,0 +1,72 @@
+import re
+#7:05
+def part1():
+    with open('aoc_2022_day05_large_input.txt') as file:
+        lines = [line.strip() for line in file.readlines()]
+        i = 0
+        all_letters = []
+        stacks = []
+        while lines[i][0] != '1':
+            letters = [part.strip().strip('[').strip(']') for part in list(chunks(lines[i], 4))]
+            all_letters.append(letters)
+            stacks.append([])
+            i += 1
+        stacks.append([])
+        i += 2
+        for row in all_letters:
+            j = 0
+            for letter in row:
+                if letter != '':
+                    stacks[j].append(letter)
+                j += 1
+
+        lines = lines[i:]
+        for line in lines:
+            m, f, t = [int(n) for n in re.findall(r'\d+', line)]
+            f -= 1
+            t -= 1
+            move_block = stacks[f][:m]
+            stacks[f] = stacks[f][m:]
+            stacks[t] = list(reversed(move_block)) + stacks[t]
+
+
+        print([stack[0] for stack in stacks])
+
+def part2():
+    with open('aoc_2022_day05_large_input.txt') as file:
+        lines = [line.strip() for line in file.readlines()]
+        i = 0
+        all_letters = []
+        stacks = []
+        while lines[i][0] != '1':
+            letters = [part.strip().strip('[').strip(']') for part in list(chunks(lines[i], 4))]
+            all_letters.append(letters)
+            stacks.append([])
+            i += 1
+        stacks.append([])
+        i += 2
+        for row in all_letters:
+            j = 0
+            for letter in row:
+                if letter != '':
+                    stacks[j].append(letter)
+                j += 1
+
+        lines = lines[i:]
+        for line in lines:
+            m, f, t = [int(n) for n in re.findall(r'\d+', line)]
+            f -= 1
+            t -= 1
+            move_block = stacks[f][:m]
+            stacks[f] = stacks[f][m:]
+            stacks[t] = move_block + stacks[t]
+
+        print([stack[0] for stack in stacks])
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+part1()
+part2()
